33750249cf985a43fa1873cef0969a51f542e3d6
Annika Paulheim <annika.paulheim@technia.com>
2021-11-16 10:17:40 +0100

    conditional rendering of ornaments

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="parent">
     <tree
-      v-for="(item, index) in days"
-      v-bind:key="`tree${index}`"
-      v-bind:x="item.x"
-      v-bind:y="item.y"
-      v-bind:index="index"
+      v-for="day in days"
+      v-bind:key="`tree${day.id}`"
+      v-bind:id="day.id"
+      v-bind:x="day.x"
+      v-bind:y="day.y"
+      v-bind:langs="day.langs"
     >
     </tree>
     <snowflakes></snowflakes>
@@ -27,30 +28,30 @@ export default {
   data() {
     return {
       days: [
-        { x: 3, y: 3 }, // 1
-        { x: 50, y: 70 }, // 2
-        { x: 8, y: 70 }, // 3
-        { x: 73, y: 70 }, // 4
-        { x: 10, y: 20 }, // 5
-        { x: 63, y: 4 }, // 6
-        { x: 40, y: 30 }, // 7
-        { x: 90, y: 15 }, // 8
-        { x: 70, y: 30 }, // 9
-        { x: 30, y: 5 }, // 10
-        { x: 50, y: 3 }, // 11
-        { x: 80, y: 3 }, // 12
-        { x: 60, y: 41 }, // 13
-        { x: 30, y: 40 }, // 14
-        { x: 90, y: 40 }, // 15
-        { x: 18, y: 5 }, // 16
-        { x: 40, y: 70 }, // 17
-        { x: 80, y: 50 }, // 18
-        { x: 63, y: 70 }, // 19
-        { x: 20, y: 30 }, // 20
-        { x: 27, y: 75 }, // 21
-        { x: 20, y: 70 }, // 22
-        { x: 40, y: 3 }, // 23
-        { x: 3, y: 40 }, // 24
+        { id: 1, x: 3, y: 3, langs: [] },
+        { id: 2, x: 50, y: 70, langs: [] },
+        { id: 3, x: 8, y: 70, langs: [] },
+        { id: 4, x: 73, y: 70, langs: [] },
+        { id: 5, x: 10, y: 20, langs: [] },
+        { id: 6, x: 63, y: 4, langs: [] },
+        { id: 7, x: 40, y: 30, langs: [] },
+        { id: 8, x: 90, y: 15, langs: [] },
+        { id: 9, x: 70, y: 30, langs: [] },
+        { id: 10, x: 30, y: 5, langs: [] },
+        { id: 11, x: 50, y: 3, langs: [] },
+        { id: 12, x: 80, y: 3, langs: [] },
+        { id: 13, x: 60, y: 41, langs: [] },
+        { id: 14, x: 30, y: 40, langs: [] },
+        { id: 15, x: 90, y: 40, langs: [] },
+        { id: 16, x: 18, y: 5, langs: [] },
+        { id: 17, x: 40, y: 70, langs: [] },
+        { id: 18, x: 80, y: 50, langs: [] },
+        { id: 19, x: 63, y: 70, langs: [] },
+        { id: 20, x: 20, y: 30, langs: [] },
+        { id: 21, x: 27, y: 75, langs: [] },
+        { id: 22, x: 20, y: 70, langs: [] },
+        { id: 23, x: 40, y: 3, langs: [] },
+        { id: 24, x: 3, y: 40, langs: [] },
       ],
     };
   },
--- src/Tree.vue
+++ src/Tree.vue
@@ -1,15 +1,15 @@
 <template>
   <div :style="`left: ${x}vw; top: ${y}vh; position: fixed;`">
-    <img :src="`trees/${index + 1}.png`" width="135" />
-    <ornament type="goorn" x="20" y="40" />
-    <ornament type="py" x="40" y="20" />
-    <ornament type="js" x="60" y="50" />
+    <img :src="`trees/${id}.png`" width="135" />
+    <ornament v-if="langs.indexOf('go') != -1" type="go" x="20" y="40" />
+    <ornament v-if="langs.indexOf('py') != -1" type="py" x="40" y="20" />
+    <ornament v-if="langs.indexOf('js') != -1" type="js" x="60" y="50" />
   </div>
 </template>
 <script>
 import Ornament from "./Ornament.vue";
 export default {
-  props: ["x", "y", "index"],
+  props: ["id", "x", "y", "langs"],
   components: {
     ornament: Ornament,
   },
