02180a869c63d3a7d570defecd4343823f72fc41
Annika Paulheim <annika.paulheim@technia.com>
2021-11-16 08:31:39 +0100

    dark mode

--- src/Snowflakes.vue
+++ src/Snowflakes.vue
@@ -1,34 +1,34 @@
 <template>
   <div class="snowflakes" aria-hidden="true">
     <div class="snowflake">
-      <img :src="`${publicPath}christmas/gift.png`" width="20" />
+      <img :src="`christmas/gift.png`" width="20" />
     </div>
     <div class="snowflake">
-      <img :src="`${publicPath}christmas/tree.png`" width="20" />
+      <img :src="`christmas/tree.png`" width="20" />
     </div>
     <div class="snowflake">
-      <img :src="`${publicPath}christmas/rudolph.png`" width="20" />
+      <img :src="`christmas/rudolph.png`" width="20" />
     </div>
     <div class="snowflake">
-      <img :src="`${publicPath}christmas/snowman.png`" width="20" />
+      <img :src="`christmas/snowman.png`" width="20" />
     </div>
     <div class="snowflake">
-      <img :src="`${publicPath}christmas/plant.png`" width="20" />
+      <img :src="`christmas/plant.png`" width="20" />
     </div>
     <div class="snowflake">
-      <img :src="`${publicPath}christmas/santa.png`" width="20" />
+      <img :src="`christmas/santa.png`" width="20" />
     </div>
     <div class="snowflake">
-      <img :src="`${publicPath}christmas/reindeer.png`" width="20" />
+      <img :src="`christmas/reindeer.png`" width="20" />
     </div>
     <div class="snowflake">
-      <img :src="`${publicPath}christmas/plant.png`" width="20" />
+      <img :src="`christmas/plant.png`" width="20" />
     </div>
     <div class="snowflake">
-      <img :src="`${publicPath}christmas/hat.png`" width="20" />
+      <img :src="`christmas/hat.png`" width="20" />
     </div>
     <div class="snowflake">
-      <img :src="`${publicPath}christmas/sock.png`" width="20" />
+      <img :src="`christmas/sock.png`" width="20" />
     </div>
   </div>
 </template>
