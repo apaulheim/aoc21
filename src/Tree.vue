694cf5518a5652a1fca8035d12681df9d4f09a7e
Annika Paulheim <annika.paulheim@gmail.com>
2021-12-15 20:25:24 +0100

    rust

--- src/Tree.vue
+++ src/Tree.vue
@@ -2,9 +2,10 @@
   <router-link :to="`/day/${id}`">
     <div class="tree" v-bind:style="styleObject">
       <img :src="`trees/${id}.png`" :width="`${width}`" />
-      <ornament v-if="langs.indexOf('go') != -1" type="go" x="20" y="40" />
+      <ornament v-if="langs.indexOf('go') != -1" type="go" x="55" y="35" />
       <ornament v-if="langs.indexOf('py') != -1" type="py" x="40" y="20" />
       <ornament v-if="langs.indexOf('js') != -1" type="js" x="60" y="50" />
+      <ornament v-if="langs.indexOf('rs') != -1" type="rs" x="20" y="40" />
     </div>
   </router-link>
 </template>
